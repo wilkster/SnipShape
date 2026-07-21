341c0db6c75b96102a0ca6b55404864d55cdf643
Wilkster <tom.wilkason@gmail.com>
2026-07-21 16:38:04 -0600

    Comments

--- MCode/SetAlphaChannel.cpp
+++ MCode/SetAlphaChannel.cpp
@@ -1,10 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-/*
-   matchAlpha = true, then full targetARGB must match and full replaceARTB is used
-              = false, then only the rgb values must match but the target alpha is used for replacement in the
-*/
+// Replace alpha channel for every pixel that doesn't have it already
 __stdcall int SetAlphaChannel(uint8_t *pixels, int width, int height, int stride, uint32_t replaceAlpha)
 {
    int replaced = 0;
@@ -19,7 +16,7 @@ __stdcall int SetAlphaChannel(uint8_t *pixels, int width, int height, int stride
       uint8_t *p = row;
       for (int x = 0; x < width; x++)
       {
-         // Ignore alpha channel
+         // Ignore matching alpha channel
          if (p[3] != ra)
          {
             p[3] = ra; // Note, Make sure replacement has a non-zero alpha channel
