16c3f4912d2a7e72e0911df43c36b0687c6826b3
Wilkster <tom.wilkason@gmail.com>
2026-07-26 10:12:22 -0600

    Rename variables for more clarity

--- MCode/ReplaceColor.cpp
+++ MCode/ReplaceColor.cpp
@@ -2,40 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 /*
-   matchAlpha = true, then full targetARGB must match and full replaceARTB is used
-              = false, then only the rgb values must match but the target alpha is used for replacement in the
+   matchAlpha = true, then full fromARGB must match and full toARTB is used
+              = false, then only the rgb values must match but the from alpha is used for toment in the
 */
-__stdcall int ReplaceColor(uint8_t *pixels, int width, int height, int stride,
-                           uint32_t targetARGB, uint32_t replaceARGB, int matchAlpha)
+__stdcall int ToColor(uint8_t *pixels, int width, int height, int stride,
+                           uint32_t fromARGB, uint32_t toARGB, int matchAlpha)
 {
    int replaced = 0;
    if (width <= 0 || height <= 0)
       return replaced;
 
-   uint8_t ta = (targetARGB >> 24) & 0xFF;
-   uint8_t tr = (targetARGB >> 16) & 0xFF;
-   uint8_t tg = (targetARGB >> 8) & 0xFF;
-   uint8_t tb = targetARGB & 0xFF;
+   uint8_t fmA = (fromARGB >> 24) & 0xFF;
+   uint8_t fmR = (fromARGB >> 16) & 0xFF;
+   uint8_t fmG = (fromARGB >> 8) & 0xFF;
+   uint8_t fmB = fromARGB & 0xFF;
 
-   uint8_t ra = (replaceARGB >> 24) & 0xFF;
-   uint8_t rr = (replaceARGB >> 16) & 0xFF;
-   uint8_t rg = (replaceARGB >> 8) & 0xFF;
-   uint8_t rb = replaceARGB & 0xFF;
+   uint8_t toA = (toARGB >> 24) & 0xFF;
+   uint8_t toR = (toARGB >> 16) & 0xFF;
+   uint8_t toG = (toARGB >> 8) & 0xFF;
+   uint8_t toB = toARGB & 0xFF;
    uint8_t *row = pixels;
-   ra = matchAlpha ? ra : ta;
-   // Replacement alpha is matchAlpha dependent
+   toA = matchAlpha ? toA : fmA;
+   // Toment alpha is matchAlpha dependent
    for (int y = 0; y < height; y++)
    {
       uint8_t *p = row;
       for (int x = 0; x < width; x++)
       {
          // Ignore alpha channel
-         if (p[0] == tb && p[1] == tg && p[2] == tr && (!matchAlpha || p[3] == ta))
+         if (p[0] == fmB && p[1] == fmG && p[2] == fmR && (!matchAlpha || p[3] == fmA))
          {
-            p[0] = rb;
-            p[1] = rg;
-            p[2] = rr;
-            p[3] = ra; // Note, Make sure replacement has a non-zero alpha channel
+            p[0] = toB;
+            p[1] = toG;
+            p[2] = toR;
+            p[3] = toA; // To with either the toment alpha or from alpha
             replaced++;
          }
          p += 4;
